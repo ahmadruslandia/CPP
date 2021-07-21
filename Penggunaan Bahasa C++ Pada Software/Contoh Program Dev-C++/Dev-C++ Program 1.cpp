fa8e99b5758484c3c502124b2896152bf244b695
Ahmad Ruslandia Papua <ruslandiaamin@gmail.com>
2021-07-21 20:24:45 +0800

    C++

---
+++ Penggunaan Bahasa C++ Pada Software/Contoh Program Dev-C++/Dev-C++ Program 1.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+using namespace std;
+
+struct Mahasiswa {
+    string MahasiswaNama, MahasiswaStambuk, MahasiswaKelas;
+};
+
+int main()
+{
+    int jumlah;
+    Mahasiswa mas[1];
+    cout << "Jumlah Mahasiswa : ";
+    cin >> jumlah;
+    for(int i = 0; i < jumlah; i++){
+        cout << "Mahasiswa ke-" << i+1 << endl;
+        cin.ignore(1, '\n');
+        cout << "Masukkan Nama    : ";
+        getline( cin, mas[i].MahasiswaNama);
+        cout << "Masukkan Stambuk : ";
+        cin >> mas[i].MahasiswaStambuk;
+        cout << "Masukkan Kelas   : ";
+        cin >> mas[i].MahasiswaKelas;
+    }
+
+    for (int i = 0; i < jumlah; i++){
+        cout << "Mahasiswa " << i+1 << " : ";
+        cout << mas[i].MahasiswaNama << "-" << mas[i].MahasiswaStambuk << "-" << mas[i].MahasiswaKelas << endl;
+    }
+
+    return 0;
+}
+
